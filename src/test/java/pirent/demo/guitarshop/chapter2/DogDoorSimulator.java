af8aa4818d76e19cfd5a55fb356a6ed19184a189
Phuc Vo <pirent420@gmail.com>
2014-06-28 17:52:21 +0700

    Update alternate path for DogDoorSimulator

--- src/test/java/pirent/demo/guitarshop/chapter2/DogDoorSimulator.java
+++ src/test/java/pirent/demo/guitarshop/chapter2/DogDoorSimulator.java
@@ -14,8 +14,10 @@ public class DogDoorSimulator {
 		remote = new Remote(door);
 	}
 	
-	@Test
+//	@Test
 	public void test() {
+		System.out.println("----- Test case 1 ------");
+		
 		System.out.println("Fido barks to go outside...");
 		remote.pressButton();
 		
@@ -24,5 +26,31 @@ public class DogDoorSimulator {
 		System.out.println("\nFido's all done...");
 		
 		System.out.println("\nFido's back inside...");
+		
+		System.out.println("------------------------");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("----- Test case 1 ------");
+		
+		System.out.println("Fido barks to go outside...");
+		remote.pressButton();
+		System.out.println("\nFido has gone outside...");
+		System.out.println("\nFido's all done...");
+		
+		try {
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e) {
+			// Pretend that Fido stay outside as long as this
+		}
+		
+		System.out.println("... but he's stuck outside");
+		System.out.println("\nFido starts barking again...");
+		System.out.println("...so Giana grabs the remote control.");
+		remote.pressButton();
+		System.out.println("\nFido's back inside...");
+		
+		System.out.println("------------------------");
 	}
 }
